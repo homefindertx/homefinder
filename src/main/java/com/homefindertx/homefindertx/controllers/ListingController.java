ce445cd77bc2f4a4c8cf564f0e090187e258cf87
charleshadden <charles.r.hadden@gmail.com>
2018-09-17 15:06:45 -0500

    added seeder

--- src/main/java/com/homefindertx/homefindertx/controllers/ListingController.java
+++ src/main/java/com/homefindertx/homefindertx/controllers/ListingController.java
@@ -4,21 +4,23 @@ package com.homefindertx.homefindertx.controllers;
 import com.homefindertx.homefindertx.repositories.ListingRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 class ListingController {
-    private final ListingRepository listingDao;
 
-    public ListingController(ListingRepository listingDao) {
-        this.listingDao = listingDao;
+    private ListingRepository listRepo;
+
+    public ListingController(ListingRepository listRepo) {
+        this.listRepo = listRepo;
     }
 
-    @GetMapping("/listings")
-    public String index(Model model) {
-        model.addAttribute("listings", listingDao.findAll());
-        return "listings/index";
+    @RequestMapping("/")
+    public String findAll(Model vModel) {
+        vModel.addAttribute("listings", listRepo.findAll());
+        return"/index";
     }
+
 
 
 }
