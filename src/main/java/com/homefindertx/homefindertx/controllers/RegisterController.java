fd3a9b8f01e71ff72c89606a32ced9e4336da9e2
charleshadden <charles.r.hadden@gmail.com>
2018-09-18 10:44:30 -0500

    added user

---
+++ src/main/java/com/homefindertx/homefindertx/controllers/RegisterController.java
@@ -0,0 +1,18 @@
+package com.homefindertx.homefindertx.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegisterController {
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
